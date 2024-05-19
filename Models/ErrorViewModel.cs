5a5dcd0f15518feef232a0acc028bed48c01f5c6
ALANIS ALESSANDRA ALVAREZ BAÑOS <2021AB601@unicaeselsalvador0.onmicrosoft.com>
2024-05-19 14:13:20 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sistema_de_tickets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
