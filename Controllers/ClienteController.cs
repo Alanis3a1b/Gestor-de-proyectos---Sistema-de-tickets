89fc1bb0bb8a6377b1e37738b4a6edd2a3b29693
DiegoBurgos <103340654+DABS03@users.noreply.github.com>
2024-05-26 18:37:11 -0600

    HomeCliente, historial de tickets, TicketTrabajado, Crear Ticket

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sistema_de_tickets.Models;
+using System.Diagnostics;
+namespace Sistema_de_tickets.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private readonly ILogger<ClienteController> _logger;
+
+        public ClienteController(ILogger<ClienteController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult CrearTicket()
+        {
+            return View();
+        }
+
+        public IActionResult HistorialDeTickets()
+        {
+            return View();
+        }
+
+        public IActionResult HomeCliente()
+        {
+            return View();
+        }
+
+        public IActionResult Settings()
+        {
+            return View();
+        }
+
+        public IActionResult TicketTrabajado()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadFile()
+        {
+            var files = HttpContext.Request.Form.Files;
+            if (files == null || files.Count == 0)
+                return BadRequest("No file uploaded.");
+
+            var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "uploads");
+            if (!Directory.Exists(uploadsFolder))
+                Directory.CreateDirectory(uploadsFolder);
+
+            var filePaths = new List<string>();
+            foreach (var file in files)
+            {
+                if (file.Length > 0)
+                {
+                    var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    var filePath = Path.Combine(uploadsFolder, fileName);
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                    filePaths.Add(fileName);
+                }
+            }
+
+            return Ok(filePaths);
+        }
+
+
+    }
+}
