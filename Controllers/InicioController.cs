73bb7358cd46f44b6c44e3dc848f31d48a7f155d
ALANIS ALESSANDRA ALVAREZ BAÑOS <alanis.alvarez@catolica.edu.sv>
2024-06-02 16:09:15 -0600

    Trabajando en la validación de credenciales... aún en proceso

---
+++ Controllers/InicioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sistema_de_tickets.Controllers
+{
+    public class InicioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
