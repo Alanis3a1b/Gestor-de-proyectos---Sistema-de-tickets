3a4dd539c2cae662501e596f7ebca418ec8576d0
ALANIS ALESSANDRA ALVAREZ BAÑOS <alanis.alvarez@catolica.edu.sv>
2024-06-03 23:02:32 -0600

    Vistas Admin para ver los tickets de todos los usuarios y se empezó la parte de "Trabajar Tickets"

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,12 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Sistema_de_tickets.Models;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.Json;
 
 namespace Sistema_de_tickets.Controllers
 {
     public class AdminController : Controller
     {
+        //Necesario hacer estos contextos antes de empezar a programar 
+        private readonly sistemadeticketsDBContext _sistemadeticketsDBContext;
+
+        public AdminController(sistemadeticketsDBContext sistemadeticketsDbContext)
+        {
+            _sistemadeticketsDBContext = sistemadeticketsDbContext;
+        }
         public IActionResult HomeAdmin()
         {
             return View();
         }
+
+        public IActionResult TodosLosTickets()
+        {
+            var todoslostickets = from t in _sistemadeticketsDBContext.tickets
+                                   join u in _sistemadeticketsDBContext.usuarios on t.id_usuario equals u.id_usuario
+                                   join e in _sistemadeticketsDBContext.estados on t.id_estado equals e.id_estado
+                                   select new
+                                   {
+                                       t.id_ticket,
+                                       t.fecha,
+                                       Usuario = u.usuario,
+                                       t.nombre_ticket,
+                                       Estado = e.nombre_estado,
+                                       AsignadoA = u.nombre
+                                   };
+
+            ViewData["TodosLosTickets"] = todoslostickets.ToList();
+
+            return View();
+        }
+
+        public IActionResult TrabajarTicketAdmin()
+        {
+            return View();
+        }
+
     }
 }
