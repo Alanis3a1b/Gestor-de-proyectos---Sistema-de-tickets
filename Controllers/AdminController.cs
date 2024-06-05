34811f4326af2bae5b7eea6c176f18479f6f19da
ALANIS ALESSANDRA ALVAREZ BAÑOS <alanis.alvarez@catolica.edu.sv>
2024-06-05 00:27:17 -0600

    Se creó la configuración para enviar correos

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Sistema_de_tickets.Models;
+using Sistema_de_tickets.Views.Services;
 using System.Diagnostics;
 using System.Linq;
 using System.Net.Sockets;
@@ -14,9 +15,13 @@ namespace Sistema_de_tickets.Controllers
         //Necesario hacer estos contextos antes de empezar a programar 
         private readonly sistemadeticketsDBContext _sistemadeticketsDBContext;
 
-        public AdminController(sistemadeticketsDBContext sistemadeticketsDbContext)
+        //Este método me permite enviar correos
+        private IConfiguration _configuration;
+
+        public AdminController(sistemadeticketsDBContext sistemadeticketsDbContext, IConfiguration configuration)
         {
             _sistemadeticketsDBContext = sistemadeticketsDbContext;
+            _configuration = configuration;
         }
         public IActionResult HomeAdmin()
         {
@@ -88,7 +93,21 @@ namespace Sistema_de_tickets.Controllers
 
 
 
-        public IActionResult CrearUsuariosAdmin()
+        public IActionResult CrearUsuariosAdmin(usuarios usuarioNuevo)
+        {
+            correo enviarCorreo = new correo(_configuration);
+            _sistemadeticketsDBContext.Add(usuarioNuevo);
+            _sistemadeticketsDBContext.SaveChanges();
+            enviarCorreo.enviar(usuarioNuevo.correo,
+                                "Cuenta para acceder a HELPHUB",
+                                "Se le ha asignado una nueva cuenta cuyo nombre de cuenta es: " + usuarioNuevo.usuario + "\n"
+                                + " Y su contraseña es:  " + usuarioNuevo.contrasenya + "\n"
+                                + " La contraseña la puede cambiar ingresando a su cuenta e ingresando luego a su perfil.");
+
+            return RedirectToAction("Success");
+        }
+
+        public IActionResult Success()
         {
             return View();
         }
---
+++ Views/Services/correo.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Sistema_de_tickets.Views.Services
+{
+    public class correo
+    {
+        private IConfiguration _configuration;
+        public correo(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        //Esta es la clase encargada de enviar los correos y en la siguiente función el método recibirá el correo destinatario,
+        //el asunto y el cuerpo del correo
+        public void enviar(string destinatario, string asunto, string cuerpo)
+        {
+            try 
+            { 
+                string connectionString = _configuration.GetSection("ConnectionStrings").GetSection("ticketsDbConnection").Value;
+                
+                string sqlQuery = "exec msdb.dbo.sp_send_dbmail " +
+                   "                 @profile_name = 'SQLMail_CATOLICA', " +
+                   "                 @recipients = @par_destinatarios, " +
+                   "                 @body = @par_asunto, " +
+                   "                 @subject = @par_mensaje ";
+
+                //COn esto ya configurado, es de usar la clase y el metodo en el controlador o metodo en donde querramos implementarlo
+                using (SqlConnection connection = new SqlConnection(connectionString)) 
+                {
+                    using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@par_destinatarios", destinatario);
+                        command.Parameters.AddWithValue("@par_asunto", asunto);
+                        command.Parameters.AddWithValue("@par_mensaje", cuerpo);
+
+                        connection.Open();  
+                        command.ExecuteNonQuery();  
+                    }
+                }
+
+            }
+            catch (Exception ex) 
+            { 
+                Console.WriteLine("Error: ");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
